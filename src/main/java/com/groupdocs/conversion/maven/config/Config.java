f8338d44ec9c8511dde93c3a417095f683822366
Alex Bobkov <lilalex85@gmail.com>
2014-11-14 11:48:28 +0000

    config

--- src/main/java/com/groupdocs/conversion/maven/config/Config.java
+++ src/main/java/com/groupdocs/conversion/maven/config/Config.java
@@ -7,14 +7,11 @@ import com.groupdocs.conversion.config.ConversionConfig;
  * @author Alex Bobkov
  */
 public class Config implements ConversionConfig{
-    private final String licensePath1 = null;
-    private final String licensePath2 = "";
-    private final String licensePath3 = "D:/Projects/GroupDocs/Apps/0-resources/licenses/aspose.total/Aspose.Total.Java.2014.lic";
-    private final String licensePath4 = "D:/Projects/GroupDocs/Apps/0-resources/licenses/groupdocs.conversion/GroupDocs.Conversion.for.Java.lic";
+    private final String licensePath = null;
     private final String savePath = "D:/Projects/GroupDocs/Apps/xFiles/converted/";
 
     public String getLicensePath() {
-        return licensePath1;
+        return licensePath;
     }
 
     public String getStoragePath() {
