76c652f6ae42718e65ef883a67d13158fa3a770c
Alex Bobkov <lilalex85@gmail.com>
2015-03-18 14:19:13 +0000

    version update

--- src/main/java/com/groupdocs/conversion/maven/domain/SampleVariables.java
+++ src/main/java/com/groupdocs/conversion/maven/domain/SampleVariables.java
@@ -8,7 +8,6 @@ import com.groupdocs.conversion.maven.config.Config;
  */
 public abstract class SampleVariables {
     protected final String PDF_FILE = "GroupDocs_Demo.pdf";
-    protected final String PDF_FILE_2 = "10_page.pdf";
     protected final String DOC_FILE = "GroupDocs_Demo.doc";
     protected final String EXCEL_FILE = "GroupDocs_Demo.xls";
     protected final String PPT_FILE = "GroupDocs_Demo.ppt";
